b348df64687f8adcd44212b3acf366874445e08b
Alex <a.holm57@hotmail.com>
2023-08-07 21:30:12 +1000

    three steps

--- githack/src/components/Home/ThreeSteps/ThreeSteps.jsx
+++ githack/src/components/Home/ThreeSteps/ThreeSteps.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./ThreeSteps.module.css";
 import TwoArrows from "../../../ui/RotatingArrows";
+import OneIcon from "../../../ui/threesteps/one";
+import TwoIcon from "../../../ui/threesteps/two";
+import ThreeIcon from "../../../ui/threesteps/three";
 
 function ThreeSteps() {
   return (
@@ -25,6 +28,49 @@ function ThreeSteps() {
       <div className={"my-5 mx-5"}>
         <div className={classes["container"]}>
           <h2 className="mb-5">3 easy steps</h2>
+          <div className={classes['cardContainer']}>
+            <div className={classes['card']}>
+            <div className={classes['cardIcon']}>
+
+            <OneIcon />
+            </div>
+              <div className={classes['graphic']} >
+              </div>
+              <p className={classes['header']}>
+              Step 1: Trade recycling for bin
+              </p>
+              <p className={classes['caption']}>
+              Lorem ipsum dolor sit amet. 33 veniam eius est impedit modi id molestiae dolore ex expedita cumque ut doloribus
+              </p>
+            </div>
+            <div className={classes['card']}>
+            <div className={classes['cardIcon']}>
+
+<TwoIcon />
+</div>
+            <div className={classes['graphic']} >
+
+            </div>
+            <p className={classes['header']}>
+            Step 2: Create account
+              </p>
+              <p className={classes['caption']}>
+              Lorem ipsum dolor sit amet. 33 veniam eius est impedit modi id molestiae dolore ex expedita cumque ut doloribus
+              </p>
+            </div>
+            <div className={classes['card']}>
+            <div className={classes['cardIcon']}>
+
+            <ThreeIcon />
+</div>
+            <div className={classes['graphic']} ></div>
+            <p className={classes['header']}>
+            Step 3: Schedule pickups
+              </p><p className={classes['caption']}>
+              Lorem ipsum dolor sit amet. 33 veniam eius est impedit modi id molestiae dolore ex expedita cumque ut doloribus 
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
